5c5738a4554dcc8beb6294b22bf3ce58493c318c
Sebastian Schürmann <sebs@2xs.org>
2025-03-07 14:26:01 +0100

    chore(release): bump version 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ecosystems-cli",
-    version="0.3.3",
+    version="0.3.4",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
