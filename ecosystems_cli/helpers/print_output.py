4825dd85459fad826ac319dfaf7cff9b1a208236
Sebastian Schürmann <sebastian.schuermann@maibornwolff.de>
2025-07-02 18:59:21 +0200

    refactor: output less complex now

--- ecosystems_cli/helpers/print_output.py
+++ ecosystems_cli/helpers/print_output.py
@@ -16,6 +16,125 @@ from ecosystems_cli.helpers.format_value import format_value
 from ecosystems_cli.helpers.parse_endpoints import flatten_dict
 
 
+def _format_json(data: Any, console: Console) -> None:
+    """Format and print data as JSON."""
+    import json
+
+    json_str = json.dumps(data, indent=2)
+    syntax = Syntax(json_str, JSON_SYNTAX, theme=JSON_THEME, line_numbers=False)
+    console.print(syntax)
+
+
+def _format_tsv(data: Any, console: Console) -> None:
+    """Format and print data as TSV (Tab-Separated Values)."""
+    if isinstance(data, list) and len(data) > 0:
+        headers = list(data[0].keys())
+        console.print("\t".join(headers))
+        for item in data:
+            console.print("\t".join(str(format_value(item.get(h, ""))) for h in headers))
+    else:
+        flat_data = flatten_dict(data) if isinstance(data, dict) else {"value": str(data)}
+        console.print("\t".join(flat_data.keys()))
+        console.print("\t".join(str(v) for v in flat_data.values()))
+
+
+def _format_jsonl(data: Any, console: Console) -> None:
+    """Format and print data as JSONL (JSON Lines)."""
+    import json
+
+    if isinstance(data, list):
+        for item in data:
+            console.print(json.dumps(item))
+    else:
+        console.print(json.dumps(data))
+
+
+def _select_table_fields(headers: list[str]) -> list[str]:
+    """Select which fields to display in the table based on priority."""
+    from typing import List
+
+    priority_fields = PRIORITY_FIELDS.copy()
+    priority_fields["common"] = ["id", "name", "title"]
+
+    # Determine API type based on available headers
+    api_type = "common"
+    for api_name, fields in priority_fields.items():
+        if api_name != "common" and any(field in headers for field in fields):
+            api_type = api_name
+            break
+
+    # Select fields based on priority
+    selected_fields: List[str] = []
+
+    # First, add priority fields for the detected API type
+    for field in priority_fields[api_type]:
+        if field in headers and len(selected_fields) < MAX_SELECTED_FIELDS:
+            selected_fields.append(field)
+
+    # If we have less than 2 fields, add common fields
+    if len(selected_fields) < 2:
+        for field in priority_fields["common"]:
+            if field in headers and field not in selected_fields and len(selected_fields) < MAX_SELECTED_FIELDS:
+                selected_fields.append(field)
+
+    # If still less than 2 fields, add any remaining headers
+    if len(selected_fields) < 2:
+        for field in headers:
+            if field not in selected_fields and len(selected_fields) < MAX_SELECTED_FIELDS:
+                selected_fields.append(field)
+
+    # Ensure we have at least 2 fields if possible
+    if len(selected_fields) == 1 and len(headers) > 0:
+        for field in headers:
+            if field not in selected_fields:
+                selected_fields.append(field)
+                break
+
+    return selected_fields
+
+
+def _format_table(data: Any, console: Console) -> None:
+    """Format and print data as a rich table."""
+    from rich.table import Table
+
+    if isinstance(data, list) and len(data) > 0:
+        # Select fields to display
+        headers = list(data[0].keys())
+        selected_headers = _select_table_fields(headers)
+
+        # Create and populate table
+        table = Table(title=DEFAULT_TABLE_TITLE, show_header=True, header_style=TABLE_HEADER_STYLE)
+        for header in selected_headers:
+            table.add_column(header.capitalize())
+
+        for item in data:
+            table.add_row(*[format_value(item.get(h, "")) for h in selected_headers])
+
+        console.print(table)
+    elif isinstance(data, dict):
+        # Create a key-value table for dict data
+        table = Table(title=DEFAULT_TABLE_TITLE, show_header=True, header_style=TABLE_HEADER_STYLE)
+        table.add_column("Field")
+        table.add_column("Value")
+
+        for key, value in data.items():
+            table.add_row(key, format_value(value))
+
+        console.print(table)
+    else:
+        # Fall back to JSON for other data types
+        _format_json(data, console)
+
+
+# Formatter registry
+_FORMATTERS = {
+    "json": _format_json,
+    "tsv": _format_tsv,
+    "jsonl": _format_jsonl,
+    "table": _format_table,
+}
+
+
 def print_output(data: Any, format_type: str = DEFAULT_OUTPUT_FORMAT, console: Console = None):
     """Print data in the specified format.
 
@@ -28,79 +147,7 @@ def print_output(data: Any, format_type: str = DEFAULT_OUTPUT_FORMAT, console: C
         from rich.console import Console
 
         console = Console()
-    if format_type == "json":
-        import json
 
-        json_str = json.dumps(data, indent=2)
-        syntax = Syntax(json_str, JSON_SYNTAX, theme=JSON_THEME, line_numbers=False)
-        console.print(syntax)
-    elif format_type == "tsv":
-        if isinstance(data, list) and len(data) > 0:
-            headers = list(data[0].keys())
-            console.print("\t".join(headers))
-            for item in data:
-                console.print("\t".join(str(format_value(item.get(h, ""))) for h in headers))
-        else:
-            flat_data = flatten_dict(data) if isinstance(data, dict) else {"value": str(data)}
-            console.print("\t".join(flat_data.keys()))
-            console.print("\t".join(str(v) for v in flat_data.values()))
-    elif format_type == "jsonl":
-        import json
-
-        if isinstance(data, list):
-            for item in data:
-                console.print(json.dumps(item))
-        else:
-            console.print(json.dumps(data))
-    else:  # Default to table
-        if isinstance(data, list) and len(data) > 0:
-            from rich.table import Table
-
-            headers = list(data[0].keys())
-            priority_fields = PRIORITY_FIELDS.copy()
-            priority_fields["common"] = ["id", "name", "title"]
-            api_type = "common"
-            for api_name, fields in priority_fields.items():
-                if api_name != "common" and any(field in headers for field in fields):
-                    api_type = api_name
-                    break
-            selected_fields = []
-            for field in priority_fields[api_type]:
-                if field in headers and len(selected_fields) < MAX_SELECTED_FIELDS:
-                    selected_fields.append(field)
-            if len(selected_fields) < 2:
-                for field in priority_fields["common"]:
-                    if field in headers and field not in selected_fields and len(selected_fields) < MAX_SELECTED_FIELDS:
-                        selected_fields.append(field)
-            if len(selected_fields) < 2:
-                for field in headers:
-                    if field not in selected_fields and len(selected_fields) < MAX_SELECTED_FIELDS:
-                        selected_fields.append(field)
-            if len(selected_fields) == 1 and len(headers) > 0:
-                for field in headers:
-                    if field not in selected_fields:
-                        selected_fields.append(field)
-                        break
-            headers = selected_fields
-            table = Table(title=DEFAULT_TABLE_TITLE, show_header=True, header_style=TABLE_HEADER_STYLE)
-            for header in headers:
-                table.add_column(header.capitalize())
-            for item in data:
-                table.add_row(*[format_value(item.get(h, "")) for h in headers])
-            console.print(table)
-        else:
-            if isinstance(data, dict):
-                from rich.table import Table
-
-                table = Table(title=DEFAULT_TABLE_TITLE, show_header=True, header_style=TABLE_HEADER_STYLE)
-                table.add_column("Field")
-                table.add_column("Value")
-                for key, value in data.items():
-                    table.add_row(key, format_value(value))
-                console.print(table)
-            else:
-                import json
-
-                json_str = json.dumps(data, indent=2)
-                syntax = Syntax(json_str, JSON_SYNTAX, theme=JSON_THEME, line_numbers=False)
-                console.print(syntax)
+    # Get the formatter function from the registry
+    formatter = _FORMATTERS.get(format_type, _format_table)
+    formatter(data, console)
--- tests/test_print_output.py
+++ tests/test_print_output.py
@@ -1,6 +1,48 @@
 from rich.console import Console
 
-from ecosystems_cli.helpers.print_output import print_output
+from ecosystems_cli.helpers.print_output import (
+    _format_json,
+    _format_jsonl,
+    _format_table,
+    _format_tsv,
+    _select_table_fields,
+    print_output,
+)
+
+
+def test_format_json_with_dict():
+    """Test that _format_json returns proper JSON syntax for dict data."""
+    from io import StringIO
+
+    data = {"foo": "bar", "baz": 1}
+    console = Console(file=StringIO(), force_terminal=True, color_system=None, width=80)
+    _format_json(data, console)
+
+    # This test will fail initially since _format_json doesn't exist yet
+
+
+def test_format_tsv_with_list():
+    """Test that _format_tsv properly formats list data as TSV."""
+    from io import StringIO
+
+    data = [{"foo": "bar", "baz": 1}, {"foo": "qux", "baz": 2}]
+    output = StringIO()
+    console = Console(file=output, force_terminal=True, color_system=None, width=80)
+    _format_tsv(data, console)
+
+    # This test will fail initially since _format_tsv doesn't exist yet
+
+
+def test_format_jsonl_with_list():
+    """Test that _format_jsonl properly formats list data as JSONL."""
+    from io import StringIO
+
+    data = [{"foo": "bar"}, {"foo": "baz"}]
+    output = StringIO()
+    console = Console(file=output, force_terminal=True, color_system=None, width=80)
+    _format_jsonl(data, console)
+
+    # This test will fail initially since _format_jsonl doesn't exist yet
 
 
 def test_print_output_json(capsys):
@@ -20,6 +62,31 @@ def test_print_output_jsonl(capsys):
     data = [{"foo": "bar"}, {"foo": "baz"}]
     console = Console(file=None, force_terminal=True, color_system=None, width=80)
     print_output(data, format_type="jsonl", console=console)
+
+
+def test_select_table_fields_with_common_fields():
+    """Test that _select_table_fields selects appropriate fields."""
+    data = [{"id": 1, "name": "foo", "extra": "data"}, {"id": 2, "name": "bar", "extra": "more"}]
+    headers = list(data[0].keys())
+
+    selected = _select_table_fields(headers)
+
+    # Should prioritize common fields like id and name
+    assert "id" in selected
+    assert "name" in selected
+    # This test will fail initially since _select_table_fields doesn't exist yet
+
+
+def test_format_table_with_list():
+    """Test that _format_table properly formats list data as table."""
+    from io import StringIO
+
+    data = [{"id": 1, "name": "foo"}, {"id": 2, "name": "bar"}]
+    output = StringIO()
+    console = Console(file=output, force_terminal=True, color_system=None, width=80)
+    _format_table(data, console)
+
+    # This test will fail initially since _format_table doesn't exist yet
 
 
 def test_print_output_table(capsys):
