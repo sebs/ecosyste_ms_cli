59e207906c2c6e008334b452beb6fc29eafe41a7
Sebastian Schürmann <sebastian.schuermann@maibornwolff.de>
2025-08-20 23:50:47 +0200

    chore: add commits api spec

--- scripts/update_api_spec.sh
+++ scripts/update_api_spec.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-curl -sL https://advisories.ecosyste.ms/docs/api/v1/openapi.yaml > apis/advisories.openapi.yaml
 curl -sL https://packages.ecosyste.ms/docs/api/v1/openapi.yaml > apis/packages.openapi.yaml
 curl -sL https://repos.ecosyste.ms/docs/api/v1/openapi.yaml > apis/repos.openapi.yaml
-curl -sL https://issues.ecosyste.ms/docs/api/v1/openapi.yaml > apis/issues.openapi.yaml
+curl -sL https://advisories.ecosyste.ms/docs/api/v1/openapi.yaml > apis/advisories.openapi.yaml
+
 curl -sL https://timeline.ecosyste.ms/docs/api/v1/openapi.yaml > apis/timeline.openapi.yaml
+curl -sL https://commits.ecosyste.ms/docs/api/v1/openapi.yaml > apis/commits.openapi.yaml
+curl -sL https://issues.ecosyste.ms/docs/api/v1/openapi.yaml > apis/issues.openapi.yaml
